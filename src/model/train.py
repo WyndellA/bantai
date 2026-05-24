5c1bfb06a52bde3f736a8e567703f209028b5405
msofiasp <142728590+msofiasp@users.noreply.github.com>
2026-05-24 11:05:54 +0800

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -3,18 +3,34 @@ import tensorflow as tf
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping
+import cv2
+import numpy as np
 
 
 IMG_SIZE = (224, 224) 
 BATCH_SIZE = 32 # num of images at a time during training
 
+def custom_augmentation(img):
+    img = np.clip(img, 0, 255).astype(np.uint8)
+
+    # Random Gaussian blur
+    if np.random.rand() < 0.5:
+        img = cv2.GaussianBlur(img, (3,3), 0)
+
+    return img.astype(np.float32)
+
 # Data augmentations
 train_datagen = ImageDataGenerator(
+    preprocessing_function=custom_augmentation,
     rescale=1./255,                     # Normalize pixel values
     validation_split=0.2,               # 20 percent for validation
     horizontal_flip=True,               # Randomly flip images (horizontally)
     rotation_range=10,                  # Randomly rotate images
-    brightness_range=[0.8, 1.2]         # Randlomly adjust brightness
+    brightness_range=[0.8, 1.2],         # Randlomly adjust brightness
+    zoom_range=0.15,
+    width_shift_range=0.1,
+    height_shift_range=0.1
 )
 
 # Validation (normalized lang dito)
@@ -49,7 +65,10 @@ base_model = MobileNetV2(
 )
 
 # Freeze base layers (to stop updating the pre-trained weights)
-base_model.trainable = False
+base_model.trainable = True
+
+for layer in base_model.layers[:-30]:
+    layer.trainable = False
 
 # Add custom layers
 x = base_model.output
@@ -59,9 +78,15 @@ output = layers.Dense(1, activation='sigmoid')(x)
 
 model = models.Model(inputs=base_model.input, outputs=output)
 
+early_stop = EarlyStopping(
+    monitor='val_loss',
+    patience=5,
+    restore_best_weights=True
+)
+
 # Compile
 model.compile(
-    optimizer='adam',
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
     loss='binary_crossentropy',
     metrics=['accuracy']
 )
@@ -70,7 +95,8 @@ model.compile(
 history = model.fit(
     train_generator,
     validation_data=val_generator,
-    epochs=10
+    epochs=20,
+    callbacks=[early_stop]
 )
 
 
